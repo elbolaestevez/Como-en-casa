287e38e421379af2c5bde00d9091c6af3c3831f5
carmelis <cacabeloscarmela@gmail.com>
2022-11-01 18:50:27 -0300

    se arregla error de compilado

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
-import "../src/assets/styles"
+import "./assets/styles/styles.scss"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
