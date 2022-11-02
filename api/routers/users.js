88558682b6b6793f8f6ca7f9894e4926ee0c09b3
juan-ma526 <jmperez675@gmail.com>
2022-11-02 12:52:52 -0300

    Crear User

---
+++ api/routers/users.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+const Users = require("../models/Users");
+
+//Creo un Usuario
+
+router.post("/register", (req, res) => {
+  Users.create(req.body)
+    .then((usuario) => res.send(usuario))
+    .catch((error) => console.log(error));
+});
+
+module.exports = router;
