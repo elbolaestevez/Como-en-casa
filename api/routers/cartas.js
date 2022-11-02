fa9fbc2d164f24df61480c71516739a2fe29d389
elbolaestevez <elbolaestevez@gmail.com>
2022-11-02 15:51:23 -0300

    actualizacion de back

--- api/routers/cartas.js
+++ api/routers/cartas.js
@@ -19,4 +19,38 @@ router.get("/", (req, res) => {
     .catch((err) => res.status(400).send(err));
 });
 
+router.post("/", (req, res) => {
+  const producto = req.body;
+
+  Cartas.create(producto)
+
+    .then((productofinal) => {
+      res.status(201).send(productofinal);
+    })
+    .catch((err) => console.log(err));
+});
+
+router.get("/", (req, res) => {
+  Cartas.findAll()
+    .then((productos) => res.status(200).send(productos))
+    .catch((err) => res.status(400).send(err));
+});
+router.get("/:name", (req, res) => {
+  Cartas.findOne({
+    where: { nombre: req.params.name },
+  })
+    .then((producto) => res.status(200).send(producto))
+    .catch((err) => res.status(400).send(err));
+});
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  Cartas.findByPk(id)
+    .then((producto) => producto.update(req.body))
+    .then((productoUpdated) => res.send(productoUpdated));
+});
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  Cartas.destroy({ where: { id } }).then(() => res.send("Producto eliminado"));
+});
+
 module.exports = router;
