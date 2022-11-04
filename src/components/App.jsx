2da0449c210c0afcde51356c78dc1b218dbd62d8
carmelis <cacabeloscarmela@gmail.com>
2022-11-04 13:25:19 -0300

    se agrego la persistencia de la sesion

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,3 @@
-import { Routes } from "react-router";
 import Login from "./Login";
 import Registro from "./Registro";
 import Navbar from "./Navbar";
@@ -6,8 +5,21 @@ import Inicio from "./Inicio";
 import Productos from "./Productos";
 import Footer from "./Footer";
 import { Route, Routes } from "react-router";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import axios from "axios";
+import { userLogin } from "../state/user";
 
 function App() {
+  
+  const dispatch = useDispatch()
+  useEffect(()=>{
+    axios.get("/api/users/me")
+    .then(res=>res.data)
+    .then(user=> dispatch(userLogin(user)))
+    .catch(()=>console.error("Falta loguearte"))
+  },[dispatch])
+
   return (
     <div>
       <Navbar />
