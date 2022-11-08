ed629f4105f3c686a05f54bac6b3d982ed42625c
Javi970 <javierpaivag@gmail.com>
2022-11-08 17:24:45 -0300

    vistaHistorialUsuario

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,31 @@
-import Login from "./Login";
-import Registro from "./Registro";
-import Navbar from "./Navbar";
-import Inicio from "./Inicio";
-import Productos from "./Productos";
-import Footer from "./Footer";
-import { Route, Routes } from "react-router";
-import ProductoDetallado from "../common/ProductoDetallado";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import axios from "axios";
-import { userLogin } from "../state/user";
+import Login from './Login';
+import Registro from './Registro';
+import Navbar from './Navbar';
+import Inicio from './Inicio';
+import Productos from './Productos';
+import Footer from './Footer';
+import { Route, Routes } from 'react-router';
+import ProductoDetallado from '../common/ProductoDetallado';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import axios from 'axios';
+import { userLogin } from '../state/user';
+import VistaHistorial from './VistaHistorial';
+
 
 function App() {
-  
-  const dispatch = useDispatch()
-  useEffect(()=>{
-    axios.get("/api/users/me")
-    .then(res=>res.data)
-    .then(user=> dispatch(userLogin(user)))
-    .catch(()=>console.error("Falta loguearte"))
-  },[dispatch])
+  const dispatch = useDispatch();
+  useEffect(() => {
+    axios
+      .get('/api/users/me')
+      .then((res) => res.data)
+      .then((user) => dispatch(userLogin(user)))
+      .catch(() => console.error('Falta loguearte'));
+  }, [dispatch]);
 
   return (
     <div>
+      
       <Navbar />
       <Routes>
         <Route path="/" element={<Inicio />}></Route>
@@ -30,11 +33,9 @@ function App() {
         <Route path="registro" element={<Registro />}></Route>
         <Route path="productos" element={<Productos />}></Route>
         <Route path="/productos/:id" element={<ProductoDetallado />}></Route>
-        {/* <Route
-          path="/productos"
-          element={<GrillaDeProductos productos={products} />}
-        ></Route> */}
+       
       </Routes>
+      <VistaHistorial/>
       <Footer />
     </div>
   );
