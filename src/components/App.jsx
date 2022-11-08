7421cdf9cc74cae2ec174d5c8ddf072909d3ef42
juan-ma526 <jmperez675@gmail.com>
2022-11-08 12:51:16 -0300

    Se corrigio donde se obtienen los productos

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,30 +10,41 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import axios from "axios";
 import { userLogin } from "../state/user";
+import { useState } from "react";
 
 function App() {
-  
-  const dispatch = useDispatch()
-  useEffect(()=>{
-    axios.get("/api/users/me")
-    .then(res=>res.data)
-    .then(user=> dispatch(userLogin(user)))
-    .catch(()=>console.error("Falta loguearte"))
-  },[dispatch])
+  const [products, setProducts] = useState([]);
+
+  //Pedido para obtener todos los productos
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/api/cartas")
+      .then((res) => res.data)
+      .then((cartas) => setProducts(cartas))
+      .catch((error) => console.error(error));
+  }, []);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    axios
+      .get("/api/users/me")
+      .then((res) => res.data)
+      .then((user) => dispatch(userLogin(user)))
+      .catch(() => console.error("Falta loguearte"));
+  }, [dispatch]);
 
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Inicio />}></Route>
+        <Route path="/" element={<Inicio products={products} />}></Route>
         <Route path="login" element={<Login />}></Route>
         <Route path="registro" element={<Registro />}></Route>
-        <Route path="productos" element={<Productos />}></Route>
+        <Route
+          path="productos"
+          element={<Productos products={products} />}
+        ></Route>
         <Route path="/productos/:id" element={<ProductoDetallado />}></Route>
-        {/* <Route
-          path="/productos"
-          element={<GrillaDeProductos productos={products} />}
-        ></Route> */}
       </Routes>
       <Footer />
     </div>
