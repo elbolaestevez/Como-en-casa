5b58ceff275b503e0b5ff63fa903eae21c3b6194
lucas h <lucas.ph@live.com>
2022-11-09 13:22:41 -0300

    se agrega superAdmin

--- api/routers/users.js
+++ api/routers/users.js
@@ -27,6 +27,8 @@ router.post("/login", (req, res) => {
           id: user.id,
           nombre: user.nombre,
           email: user.email,
+          tipo: user.tipo,
+          superAdmin: user.superAdmin,
           acumulado: user.acumulado,
         };
         // genero el token con el payload creado
