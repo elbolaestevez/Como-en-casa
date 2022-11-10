6497ff7ede4bf528df24fc95c609c17f6ad764e5
Rafa Mojica <rafael.mojica27@gmail.com>
2022-11-10 18:07:30 -0500

    Se arregla bug en eliminar  producto carrito

---
+++ src/state/carrito.js
@@ -0,0 +1,19 @@
+import { createAsyncThunk, createReducer } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = []
+
+export const carritoProducto = createAsyncThunk("OBTENER_CARRITO", async (email)=>{
+    try {
+        const productos = await axios.get(`/api/carrito/${email}`)
+        return productos.data
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+const carritoReducer = createReducer(initialState, {
+  [carritoProducto.fulfilled]: (state, action) => action.payload
+});
+
+export default carritoReducer;
